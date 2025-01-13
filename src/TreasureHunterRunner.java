8ed4ade79a5611e75412f85a025c309c7e70534f
BT_1N3_31 <sample@bths.edu>
2025-01-13 10:14:00 -0500

    added print

--- src/TreasureHunterRunner.java
+++ src/TreasureHunterRunner.java
@@ -1,5 +1,6 @@
 public class TreasureHunterRunner {
     public static void main(String[] args) {
+        System.out.println();
         TreasureHunter game = new TreasureHunter();
         game.play();
     }
